571b3f101fa9b83a502ccea048b45233f6d63b42
theologiae <mike.stacey@jamersan>
2019-01-26 11:52:32 -0700

    updated theme name

--- theme/registration.php
+++ theme/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/LearnJs/theme',
+    'frontend/LearningJsTheme/theme',
     __DIR__
 );
